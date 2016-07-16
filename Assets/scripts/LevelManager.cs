a2bfc30e52b060e4b2fb65d2c4064f268373f4d9
Zhanzhan He <tomhe123@hotmail.com>
2016-07-16 15:11:13 +0100

    Added level manager

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public float autoLoadNextLevelAfter;
+	
+	void Start () {
+		if (autoLoadNextLevelAfter > 0)
+		{
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void LoadLevel(string name) {
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Application.Quit();
+	}
+}
