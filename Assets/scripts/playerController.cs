d859d58524db24f93c5723fa629e92b33c163de5
June He <junehe1999@gmail.com>
2016-07-19 23:37:49 +0100

    player animation - kind of broken but you can move

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float maxSpeed;
+	Rigidbody2D myRB;
+	Animator theAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myRB = GetComponent<Rigidbody2D>();
+		theAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+		theAnimator.SetFloat("speed", move);
+		myRB.velocity = new Vector2(move*maxSpeed, myRB.velocity.y);
+		
+	}
+	
+}
